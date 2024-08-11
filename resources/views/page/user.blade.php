65001f2ded3d58f03b4e05193e867b6e39bd5e9d
nnqn15 <nnqn15@gmail.com>
2024-08-11 11:53:09 +0700

    admin add user layout

--- resources/views/page/user.blade.php
+++ resources/views/page/user.blade.php
@@ -9,6 +9,40 @@ Nhân viên
             <h2>
                 Nhân viên
             </h2>
+            <div class="text-end">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Thêm nhân viên</button>
+            </div>
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Thêm nhân viên mới</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                                <div class="form-floating mb-3">
+                                    <input type="text" name="name" class="form-control" id="floatingInputName" placeholder="Nguyễn Văn A">
+                                    <label for="floatingInputName">Tên nhân viên</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="email" name="email" class="form-control is-invalid" id="floatingInput" placeholder="name@example.com">
+                                    <label for="floatingInput">Email</label>
+                                </div>
+                                <div class="form-floating">
+                                    <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                    <label for="floatingPassword">Mật khẩu</label>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Send message</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             @if (!$users)
             <div class="alert alert-primary" role="alert">
                 Chưa có nhân viên
